f067b8f0d40d241e64e5aa3dd3f0ecf500aaeebf
aaryanghadi12 <aaryan.ghadi12@gmail.com>
2024-03-07 12:28:12 +0530

    Hello World

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+email=st.text_input('Enter email')
+password=st.text_input('Enter Password')
+
+
+btn = st.button('login karo')
+
+
+#
+if btn:
+    if email == 'amangupta@gmail.com' and password == '12345':
+        st.snow()
+
+    else:
+        st.error('Login Failed')
+
